c49a97c90212c3e27cd66ad086d321ab13ba810c
Alex_Beng <599267065@qq.com>
2021-03-23 16:55:32 +0800

    update pc_serv.json & dnn send-recv test passed

--- PcServer/dnn_serv.py
+++ PcServer/dnn_serv.py
@@ -10,17 +10,21 @@ from Util.util import readjson
 
 
 def dnn_server(sock: socket.socket):
-    data = sock.recv(102400000)
+    data = b''
+    for i in range(20):
+        data += sock.recv(102400)
+    
+    print(f'receive: {len(data)}')
     if not data:
         sock.close()
         return
     img = np.asarray(bytearray(data), dtype="uint8")
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    cv2.imshow("ya", img)
+    cv2.imshow(f"ya{sock.getsockname()}", img)
     cv2.waitKey()
+    cv2.destroyAllWindows()
     res = ""
     sock.send(bytes(res, encoding='utf-8'))
-    sock.close()
 
 
 def main(cofig_file, font_cd='utf-8', cfg_cd='utf-8') -> int:
