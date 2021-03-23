f0c6397da4aaa1c347780d8df05d0c00b97e1837
Alex_Beng <599267065@qq.com>
2021-03-24 00:23:37 +0800

    update pc server to udp

--- PcServer/dnn_serv.py
+++ PcServer/dnn_serv.py
@@ -2,43 +2,42 @@ import socket
 import json
 import threading
 import cv2
+import time
 import numpy as np
 
 import sys
 sys.path.append("../")
-from Util.util import readjson
+from Util.util import readjson, bytes2img
 
+def dnn_server_udp(sock: socket.socket):
+    while True:
+        begin_time = time.time()
 
-def dnn_server(sock: socket.socket):
-    data = b''
-    for i in range(20):
-        data += sock.recv(102400)
-    
-    print(f'receive: {len(data)}')
-    if not data:
-        sock.close()
-        return
-    img = np.asarray(bytearray(data), dtype="uint8")
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    cv2.imshow(f"ya{sock.getsockname()}", img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    res = ""
-    sock.send(bytes(res, encoding='utf-8'))
+        data, addr = sock.recvfrom(1024000)
+        
+        print(f'receive: {len(data)} from {addr}')
 
+        img = bytes2img(data)
+        end_time = time.time()
+        cv2.putText(img, '%.2f fps'%(1.0/(end_time-begin_time)), 
+                    (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
+        cv2.imshow(f"ya{sock.getsockname()}", img)
+        cv2.waitKey(1)
+
+        
+
+        
+        
 
 def main(cofig_file, font_cd='utf-8', cfg_cd='utf-8') -> int:
     config = readjson(cofig_file, cfg_cd)
 
-    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # in case CTRLC
 
     s.bind((config['addr'], config['port']))
-    s.listen(socket.SOMAXCONN)
-    while True:
-        sock, addr = s.accept()
-        t = threading.Thread(target=dnn_server, args=[sock])
-        t.start()
+    dnn_server_udp(s)
+
 
 if __name__ == "__main__":
     main(
