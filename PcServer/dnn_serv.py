b4c5df88122d2e611d58526ddef6eba20070bac2
Alex_Beng <599267065@qq.com>
2021-03-25 00:18:49 +0800

    pc server fix undefine var in udp-once func

--- PcServer/dnn_serv.py
+++ PcServer/dnn_serv.py
@@ -12,7 +12,7 @@ from Util.util import readjson, bytes2img
 def dnn_server_udp_once(config):
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     s.bind((config['addr'], config['port']))
-    data, addr = sock.recvfrom(1024000)
+    data, addr = s.recvfrom(1024000)
 
     print(f'receive: {len(data)} from {addr}')
 
