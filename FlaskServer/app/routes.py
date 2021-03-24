1b34ccfa64be430af81127dd30f132448777d562
Alex_Beng <599267065@qq.com>
2021-03-24 20:49:35 +0800

    init flask structure

--- FlaskServer/app/routes.py
+++ FlaskServer/app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template, flash, redirect, url_for, request, send_from_directory, abort
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+
+    return render_template('index.html', title='Home')
+
+@app.route('/select')
+def select():
+    return render_template('select.html', title='Home')
+
+@app.route('/add')
+def add():
+    return render_template('add.html', title='Home')
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html', title='Home')
